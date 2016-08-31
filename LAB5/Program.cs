604ffaaf6979cd2ad2e692cfbc2d7575d2856247
moeh1234 <moeh1234@gmail.com>
2016-08-31 13:56:18 -0400

    Add project files.

---
+++ LAB5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
